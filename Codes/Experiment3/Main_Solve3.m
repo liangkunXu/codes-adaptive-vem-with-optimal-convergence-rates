64adef65d82a008c44a3f0ddfbd8a3f414cbda1a
liangkunXu <780261755@qq.com>
2026-09-22 09:59:59 +0800

    Update Main_Solve3.m

--- Codes/Experiment3/Main_Solve3.m
+++ Codes/Experiment3/Main_Solve3.m
@@ -2,6 +2,7 @@ function [etaN,dofN,eigenN,NT]=Main_Solve3()
 %% Adaptive Virtual Element Method
 %% Parameters
 theta = 0.5;  Tol = 7e-03; epsrong=1; %Tol = 3e-04
+Lambda = 1; meshState = []; 
 %% S-shaped region with cracks
 node = [0,1;0.5,1;1,1;0,0.5;0.5,0.5;1,0.5;0,0;0.5,0;1,0;1,0.5];
 elem=[1 4 2; 5 2 4; 2 5 3; 6 3 5; 4 7 5; 8 5 7;5 8 10;9 10 8];
@@ -59,16 +60,17 @@ while epsrong > 0.5*Tol
             % Step 3: MARK
             elemMarked = mark(elem,eta1.^0.5,theta);
             % Step 4: REFINE
-            bdFlag = SetBoundary(node,elem,'Dirichlet');
-            [node,elem,~,~,HB,belong]=bisect(node,elem,elemMarked,bdFlag);
+            %bisect now returns a nonconforming, Lambda-admissible
+            % polygonal mesh; boundary nodes are found later by setboundary.
+            [node,elem,~,~,HB,belong,meshState]= ...
+                bisect(node,elem,elemMarked,[],meshState,Lambda);
             showmesh(node,elem);
             pause(0.025);
-            jishi=jishi+1;
+            jishi=jishi+1;        
         end
     end
     epsrong=0.5*epsrong;
     k=k+1;
+    fprintf(' 第 %d 次外迭代开始\n', k);
 end
-%uhh=uh(:,2);
-%showsolution(node,elem,uhh(1:size(node,1),1));
 end
